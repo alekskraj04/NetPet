3af258e6aa2ecf61619aaf186709494881e00df6
alekskraj04 <alekskraj04@gmail.com>
2026-03-08 20:37:20 +0100

    Final fix: Removed missing favicon from cache and updated icon paths

--- client/sw.js
+++ client/sw.js
@@ -4,22 +4,20 @@ const ASSETS_TO_CACHE = [
   './index.html',
   './css/app.css',
   './app.mjs',
-  './assets/favicon.png',
-  './assets/icon-192.png', 
+  './assets/icon-192.png', // Denne finnes, så denne er trygg!
   './views/UserView.html',
   './views/GAMEVIEW.html'
 ];
 
-// Installerer Service Worker og lagrer filer i cache
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      // Nå vil denne fullføre uten feil fordi alle filene eksisterer
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
-// Henter filer fra cache hvis vi er offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
